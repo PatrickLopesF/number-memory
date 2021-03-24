48b1816fb0c1bfa83b856795401d5d1dc55ee201
Patrick Lopes <79598195+PatrickLopesF@users.noreply.github.com>
2021-03-24 15:09:42 -0300

    Add files via upload

--- memorygame.py
+++ memorygame.py
@@ -5,7 +5,7 @@ list = []
 round = 1
 
 
-GAME_DIF = input("Choose your difficulty!\na)Easy\nb)Medium\nc)Hard\nd)Impossible ")
+GAME_DIF = input("Choose your difficulty!\na)Easy\nb)Medium\nc)Hard\nd)Impossible\n\n ")
 
 if GAME_DIF == "a":
     n = 2.5
@@ -21,13 +21,17 @@ else:
 def clear():
     if name == 'nt':
         _ = system('cls')
-
+sleep(n)
+clear()
 list.append(random.randint(1, 9))
 x = ''.join(str(e) for e in list)
 print(str(x))
-
+sleep(n)
+clear()
 y = input("Number: ")
+clear()
 x = ''.join(str(e) for e in list)
+
 while x == y:
     del list[:]    
     for i in range(0, round + 1):                  
@@ -43,5 +47,55 @@ while x == y:
 if x != y:
     print("You lost!")
     sleep(2.5)
+    clear()
+AGAIN = input("Do you want to play again?\nEnter Y for YES or N for NO ")
+
+while AGAIN == "Y":
+    list = []
+    round = 1
+
+
+    GAME_DIF = input("Choose your difficulty!\na)Easy\nb)Medium\nc)Hard\nd)Impossible\n\n ")
+
+    if GAME_DIF == "a":
+        n = 2.5
+    elif GAME_DIF == "b":
+        n = 1.75
+    elif GAME_DIF == "c":
+        n = 1
+    elif GAME_DIF == "d":
+        n = 0.5
+    else:
+        print("Invalid input.")
+
+    
+    sleep(n)
+    clear()
+    list.append(random.randint(1, 9))
+    x = ''.join(str(e) for e in list)
+    print(str(x))
+    sleep(n)
+    clear()
+    y = input("Number: ")
+    clear()
+    x = ''.join(str(e) for e in list)
+
+    while x == y:
+        del list[:]    
+        for i in range(0, round + 1):                  
+            list.append(random.randint(1, 9))
+        x = ''.join(str(e) for e in list)
+        print(x)
+        sleep(n)
+        clear()
+        y = input("Number: ")
+        clear()
+        round += 1
+
+    if x != y:
+        print("You lost!")
+        sleep(2)
+        clear()
+    AGAIN = input("Do you want to play again?\nEnter Y for YES or N for NO ")
 
 exit()
